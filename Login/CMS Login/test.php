0a9fc0d22e3274f5d4e9068e4161b62086f5b276
Franziska <Franziska@DESKTOP-EK2AUJ2>
2018-11-27 21:12:00 +0100

    Added login to cms login page.

---
+++ Login/CMS Login/test.php
@@ -0,0 +1,61 @@
+<?php
+	ob_start();
+	session_start(); 
+
+	$host="localhost"; // Host name 
+	$username="root"; // Mysql username 
+	$password=""; // Mysql password 
+	$db_name="cms_blog"; // Database name 
+	$tbl_name="user"; // Table name
+
+	// Connect to server and select databse.
+	$connnection = mysqli_connect("$host", "$username", "$password");
+	
+	if (mysqli_connect_errno())
+	{
+		
+	}
+	else 
+	{
+	}
+	
+	mysqli_select_db($connnection, "$db_name");
+	// Check $username and $password 
+
+	if(empty($_POST['usermail']))
+	{
+		return false;
+	}
+	if(empty($_POST['password']))
+	{
+		return false;
+	}
+
+
+	// Define $username and $password 
+	$username=$_POST['usermail']; 
+	$password=md5($_POST['password']);
+
+
+	// To protect MySQL injection (more detail about MySQL injection)
+	$username = stripslashes($username);
+	$password = stripslashes($password);
+	$password = crypt($password, "$6$498484");
+
+	$sql="SELECT * FROM user WHERE user_email='$username' and user_password='$password'";
+	
+	$result=mysqli_query($connnection, $sql);
+
+	// Mysql_num_row is counting table row
+	$count=mysqli_num_rows($result);
+	// If result matched $username and $password, table row must be 1 row
+	if ($count==1) {
+		$_SESSION["login"] = 1; 
+		echo "session succeeded";
+		include("backend.php"); 
+	} else {
+		include("index.php"); 
+	}
+
+	ob_end_flush();
+?>
